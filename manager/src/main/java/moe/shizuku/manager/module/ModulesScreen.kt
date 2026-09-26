01d6a08cd6477d9ae8814dbf168062c2c6aed4fa
arys <arys@laptop>
2026-09-26 23:25:55 +0300

    fix(build): add missing DisposableEffect import to ModulesScreen

--- manager/src/main/java/moe/shizuku/manager/module/ModulesScreen.kt
+++ manager/src/main/java/moe/shizuku/manager/module/ModulesScreen.kt
@@ -61,6 +61,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
